8c18f8c182108601c9268a5a617036605ee2e50a
hiser2009 <hiser2002@yahoo.com>
2023-06-24 00:08:06 -0700

    add pytesting.

---
+++ test_app.py
@@ -0,0 +1,28 @@
+import pytest
+from flask import template_rendered
+from app import app
+
+@pytest.fixture
+def captured_templates(app):
+    templates = []
+
+    def capture(sender, template, context, **extra):
+        templates.append(template)
+
+    template_rendered.connect(capture, app)
+
+    try:
+        yield templates
+    finally:
+        template_rendered.disconnect(capture, app)
+
+def test_index_route(client, captured_templates):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert len(captured_templates) == 1
+    template, context = captured_templates[0]
+    assert template.name == 'index.html'
+    assert 'events' in context
+    assert 'pst_formatted' in context
+
+
